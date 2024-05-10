eb86b24d7a20430f1525e064a490e26346d80688
JustinBaconPH <justinbaconph@gmail.com>
2024-05-10 16:03:01 +0800

    feat: Added Reviews backend + ReviewBox component

---
+++ backend/reviewsapi.php
@@ -0,0 +1,70 @@
+<?php
+include 'connection.php';
+
+// Set headers for allowing CORS and specifying JSON content type
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+
+// Handle GET requests
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Retrieve all records
+    if ($_GET['action'] === 'get_all') {
+        $stmt = $conn->prepare("SELECT * FROM reviews WHERE is_deleted = 0");
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($rows);
+    }
+
+    // Retrieve all deleted records
+    if ($_GET['action'] === 'get_all_deleted') {
+        $stmt = $conn->prepare("SELECT * FROM reviews WHERE is_deleted = 1");
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($rows);
+    }
+
+    // Retrieve username based on user_id on the review
+    if ($_GET['action'] === 'get_by_id' && isset($_GET['review_id'])) {
+        $review_id = $_GET['review_id'];
+        $stmt = $conn->prepare("SELECT reviews.*, users.username AS username FROM reviews LEFT JOIN users ON reviews.user_id = users.user_id WHERE review_id = :review_id");
+        $stmt->bindParam(':review_id', $review_id);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($row);
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Archive
+    if ($data['action'] === 'soft_archive') {
+        $review_id = $data['review_id'];
+
+        try {
+            $stmt = $conn->prepare("UPDATE reviews SET is_deleted = 1 WHERE review_id = :review_id");
+            $stmt->bindParam(':review_id', $review_id);
+            $stmt->execute();
+            echo json_encode(array("message" => "Review archived successfully"));
+        } catch (PDOException $e) {
+            echo json_encode(array("error" => $e->getMessage()));
+        }
+    }
+
+    // Unarchive
+    if ($data['action'] === 'soft_unarchive') {
+        $review_id = $data['review_id'];
+
+        try {
+            $stmt = $conn->prepare("UPDATE reviews SET is_deleted = 0 WHERE review_id = :review_id");
+            $stmt->bindParam(':review_id', $review_id);
+            $stmt->execute();
+            echo json_encode(array("message" => "Review unarchived successfully"));
+        } catch (PDOException $e) {
+            echo json_encode(array("error" => $e->getMessage()));
+        }
+    }
+}
+?>
---
+++ src/components/ReviewBox.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="review-box">
+        <div class="review-ratings-container">
+            <img v-for="star in review.review_stars" :key="star" src="@/assets/icons/star.svg" alt=""
+                class="review-star" />
+        </div>
+        <div class="review-comment mx-auto">
+            <p class="text-overflow text-left">{{ review.review_content }}</p>
+        </div>
+
+        <div class="review-profile">
+            <div class="profile-container">
+                <img src="@/assets/rovick.jpg" alt="" class="user-pic" />
+            </div>
+            <div class="name-container">
+                <p>{{ user?.username }}</p>
+                <p class="client">Client</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    review: Object
+});
+
+const user = ref(null);
+
+onMounted(async () => {
+    try {
+        const response = await axios.get(`http://localhost/GRP5_MIDNIGHTS/backend/reviewsapi.php?action=get_by_id&review_id=${props.review.user_id}`);
+        user.value = response.data;
+    } catch (error) {
+        console.error('Error fetching user information:', error);
+    }
+});
+</script>
+
+<style scoped>
+.review-container {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    margin-top: 1rem;
+    background-color: transparent;
+    width: 90%;
+    margin: auto;
+    margin-bottom: 1.2rem;
+    padding: 2rem
+}
+
+.review-container {
+    background-color: transparent;
+    height: auto;
+    justify-content: center;
+}
+
+.review-box {
+    border: 0.1rem solid #FFF;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    border-radius: 2rem;
+    margin: 0.8rem;
+    flex-direction: column;
+    padding: 1.2rem;
+}
+
+.review-ratings-container {
+    flex: 1;
+    background-color: transparent;
+    margin-top: 1rem;
+}
+
+.review-comment {
+    font-weight: bold;
+    color: #FFF;
+    font-size: 1.2rem;
+    font-family: 'Poppins', 'Montserrat', sans-serif;
+    margin-top: 1rem;
+    line-height: 1.4;
+    word-break: break-all;
+    max-height: calc(1.3rem * 7);
+    overflow: hidden;
+    position: relative;
+}
+
+.review-comment::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 1.4rem;
+    background: linear-gradient(to bottom, transparent, #000 50%);
+}
+
+.review-profile {
+    flex: 0.5;
+    /* border: 1px solid #fff; */
+    display: flex;
+    flex-direction: row;
+    margin-top: 1.5rem;
+}
+
+.profile-container {
+    flex: 0.1;
+    background-color: transparent;
+    display: flex;
+
+}
+
+.name-container {
+    flex: 0.9;
+    /* background-color:pink; */
+    padding: 0 0 0 0.4rem;
+    /*top, right, bottom, left*/
+    font-family: 'Poppins', 'Montserrat', sans-serif;
+    font-weight: bold;
+    justify-content: center;
+    flex-direction: column;
+    display: flex;
+    font-size: 1.1rem;
+    color: #FFF;
+}
+
+.name-container>p {
+    padding: 0.17rem 0.4rem;
+    margin: 0;
+    /* border:1px solid #000; */
+    color: #FFF;
+}
+
+.user-pic {
+
+    height: 4.5rem;
+    width: 4.5rem;
+    border-radius: 0.4rem;
+}
+
+.client {
+    font-weight: normal;
+    color: #FFF;
+}
+
+.review-star {
+    margin-right: 0.3rem;
+}
+</style>
