ba9a7f315caff25f0393b9c408eeb1f1b159a8c2
JustinBaconPH <justinbaconph@gmail.com>
2024-05-11 05:09:23 +0800

    updated password

--- backend/connection.php
+++ backend/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "midnights";
+$servername = "sql107.infinityfree.com";
+$username = "if0_36523965";
+$password = "7G3SJKjJn6Lns";
+$database = "if0_36523965_midnights";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
